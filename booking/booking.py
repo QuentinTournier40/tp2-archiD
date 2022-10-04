6769106febad0bb8a39c52c5b0789de091303842
QuentinTournier40 <qtournier@iutbayonne.univ-pau.fr>
2022-10-04 18:10:43 +0200

    ajout question 2

--- booking/booking.py
+++ booking/booking.py
@@ -5,16 +5,48 @@ from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
-PORT = 3003
+PORT = 3201
 HOST = '0.0.0.0'
 
-with open('{}/databases/bookings.json'.format("."), "r") as jsf:
+with open('{}/data/bookings.json'.format("."), "r") as jsf:
    bookings = json.load(jsf)["bookings"]
 
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings", methods=['GET'])
+def get_json():
+   return make_response(jsonify(bookings), 200)
+
+# Fonction crée par Tournier Quentin et Marche Jules
+# But: Afficher toutes les réservations d'un Utilisateur
+# En entrée: UserId(path)
+# En sortie: Un tableau de toutes les réservations
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         return make_response(jsonify(booking), 200)
+   return make_response(jsonify({"error":"booking with this id not found"}),400)
+
+
+
+# TODO Ajouter FILM à la réservation (date/ID) + Verification de que le film ne soit pas ajouté à la même date pour cette personne
+# Fonction crée par Tournier Quentin et Marche Jules
+# But: Ajouter une réservation à un utilisateur
+# En entrée: UserId(path)
+# En sortie: Un message stipulant l'ajout de la réservation
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid):
+   req = request.get_json()
+
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         return make_response(jsonify({"error": "booking ID already exists"}), 409)
+
+   bookings.append(req)
+   return make_response(jsonify({"message": "booking added"}), 200)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
--- user/user.py
+++ user/user.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
-from werkzeug.exceptions import NotFound
 
 import grpc
-from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
-import movie_pb2
 import movie_pb2_grpc
+import movie_pb2
 
 app = Flask(__name__)
 
@@ -17,6 +13,39 @@ HOST = '0.0.0.0'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
+
+def get_movie_by_id(stub, id):
+   return stub.GetMovieByID(id)
+@app.route("/get-movies-infos-of-selected-movies-by-userid/<userid>", methods=['GET'])
+def get_movies_infos_of_selected_movies_by_userid(userid):
+   moviesInfos = json.loads('{"movies": []}')
+   for user in users:
+      if str(user["id"]) == str(userid):
+         booking = requests.get("http://172.20.190.169:3201/bookings/" + userid)
+
+   if not 'booking' in locals():
+      return make_response(jsonify({"error": "user for this userid not found"}), 400)
+
+   if booking.status_code != 200:
+      return make_response(jsonify({"error": "this userid doesn't have bookings"}), 400)
+
+   with grpc.insecure_channel('localhost:3001') as channel:
+      stub = movie_pb2_grpc.MovieStub(channel)
+
+      for date in booking.json()["dates"]:
+         for movieid in date["movies"]:
+            id = movie_pb2.MovieID(id=movieid)
+            movie = get_movie_by_id(stub, id)
+            obj = {
+               "id" : movie.id,
+               "title" : movie.title,
+               "rating" : movie.rating,
+               "director" : movie.director
+            }
+            moviesInfos["movies"].append(obj)
+   channel.close()
+
+   return make_response(jsonify(moviesInfos), 200)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
