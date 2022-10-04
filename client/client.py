52cbc205bae9076a7ae6c43838e94ff7dd839a48
QuentinTournier40 <qtournier@iutbayonne.univ-pau.fr>
2022-10-04 16:51:52 +0200

    Quentin: ByTitle / CreateMovie

--- client/client.py
+++ client/client.py
@@ -8,6 +8,19 @@ def get_movie_by_id(stub,id):
     movie = stub.GetMovieByID(id)
     print(movie)
 
+def get_list_movies(stub):
+    allmovies = stub.GetListMovies(movie_pb2.Empty())
+    for movie in allmovies:
+        print("Movie called %s" % (movie.title))
+
+def get_movie_by_title(stub, title):
+    movie = stub.GetMovieByTitle(title)
+    print(movie)
+
+def create_movie(stub, movie):
+    res = stub.CreateMovie(movie)
+    print(res)
+
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
@@ -16,8 +29,23 @@ def run():
         stub = movie_pb2_grpc.MovieStub(channel)
 
         print("-------------- GetMovieByID --------------")
-        movieid = movie_pb2.MovieID(id="a8034f44-aee4-44cf-b32c-74cf452aaaae")
+        movieid = movie_pb2.MovieID(id="720d006c-3a57-4b6a-b18f-9b713b073f3c")
         get_movie_by_id(stub, movieid)
+
+        print("-------------- GetAllMovies --------------")
+        get_list_movies(stub)
+
+        print("-------------- GetMovieByTitle --------------")
+        movieTitle = movie_pb2.MovieTitle(title="The Good Dinosaur")
+        get_movie_by_title(stub, movieTitle)
+
+        print("-------------- CreateMovie --------------")
+        movie = movie_pb2.MovieData(title="Ma vie", director="Ma vie", id="0", rating=100000)
+        create_movie(stub, movie)
+
+
+
+
 
     channel.close()
 
--- movie/movie.py
+++ movie/movie.py
@@ -4,11 +4,47 @@ import movie_pb2
 import movie_pb2_grpc
 import json
 
+with open('{}/data/movies.json'.format("."), "r") as jsf:
+   movies = json.load(jsf)["movies"]
+
 class MovieServicer(movie_pb2_grpc.MovieServicer):
 
     def __init__(self):
         with open('{}/data/movies.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["movies"]
+
+    def GetMovieByID(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def GetListMovies(self, request, context):
+        for movie in self.db:
+            yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                      id=movie['id'])
+
+    def GetMovieByTitle(self, request, context):
+        for movie in self.db:
+            if movie["title"] == request.title:
+                return movie_pb2.MovieData(title=movie["title"], rating=movie['rating'], director=movie['director'],
+                                      id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def CreateMovie(self, request, context):
+        newMovie = {
+            "id" : request.id,
+            "title" : request.title,
+            "rating" : request.rating,
+            "director" : request.director
+        }
+        for movie in self.db:
+            if movie["id"] == newMovie["id"]:
+                return movie_pb2.NotificationMessage(message="Movie id already exist")
+        self.db.append(newMovie)
+        return movie_pb2.NotificationMessage(message="Movie added!")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
