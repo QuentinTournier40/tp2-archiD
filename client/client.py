7ec8f43c41056acfc954222aac02b495d4219f79
Jules <jmarchepro@gmail.com>
2022-10-04 16:51:08 +0200

    Jules: ByTitle / ByDirector / PutRating

--- client/client.py
+++ client/client.py
@@ -4,9 +4,31 @@ import movie_pb2
 import movie_pb2_grpc
 
 
-def get_movie_by_id(stub,id):
+def get_movie_by_id(stub, id):
     movie = stub.GetMovieByID(id)
     print(movie)
+
+
+def get_list_movies(stub):
+    allmovies = stub.GetListMovies(movie_pb2.Empty())
+    for movie in allmovies:
+        print("Movie called %s" % (movie.title))
+
+
+def get_movie_by_title(stub, title):
+    movie = stub.GetMovieByTitle(title)
+    print(movie)
+
+
+def get_movie_by_director(stub, director):
+    movie = stub.GetMovieByDirector(director)
+    print(movie)
+
+
+def update_movie_rating(stub, movieIdRating):
+    movie = stub.UpdateMovieRating(movieIdRating)
+    print(movie)
+
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -19,7 +41,20 @@ def run():
         movieid = movie_pb2.MovieID(id="a8034f44-aee4-44cf-b32c-74cf452aaaae")
         get_movie_by_id(stub, movieid)
 
+        print("-------------- GetMovieByTitle --------------")
+        movieTitle = movie_pb2.MovieTitle(title="The Martian")
+        get_movie_by_title(stub, movieTitle)
+
+        print("-------------- GetMovieByDirector --------------")
+        movieTitle = movie_pb2.MovieDirector(director="Jonathan Levine")
+        get_movie_by_director(stub, movieTitle)
+
+        print("-------------- UpdateMovieRating --------------")
+        movieIdRating = movie_pb2.MovieIdRating(id="267eedb8-0f5d-42d5-8f43-72426b9fb3e6", rating=14.2)
+        update_movie_rating(stub, movieIdRating)
+
     channel.close()
+
 
 if __name__ == '__main__':
     run()
--- movie/movie.py
+++ movie/movie.py
@@ -4,11 +4,56 @@ import movie_pb2
 import movie_pb2_grpc
 import json
 
+
+with open('{}/data/movies.json'.format("."), "r") as jsf:
+   movies = json.load(jsf)["movies"]
+
+
 class MovieServicer(movie_pb2_grpc.MovieServicer):
 
     def __init__(self):
         with open('{}/data/movies.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["movies"]
+
+    def GetMovieByID(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def GetListMovies(self, request, context):
+        for movie in self.db:
+            yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                      id=movie['id'])
+
+    def GetMovieByTitle(self, request, context):
+        for movie in self.db:
+            if movie['title'] == request.title:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def GetMovieByDirector(self, request, context):
+        for movie in self.db:
+            if movie['director'] == request.director:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def UpdateMovieRating(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                movie['rating'] = request.rating
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
