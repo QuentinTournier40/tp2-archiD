e66ece1b43c16dc780d21a2464bb29a7142e765e
QuentinTournier40 <qtournier@iutbayonne.univ-pau.fr>
2022-10-08 12:26:42 +0200

    refacto de user

--- user/user.py
+++ user/user.py
@@ -1,41 +1,50 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
+from flask import Flask, jsonify, make_response
+from google.protobuf.json_format import MessageToJson
 import json
 
 import grpc
 import movie_pb2_grpc
 import movie_pb2
+import booking_pb2
+import booking_pb2_grpc
 
 app = Flask(__name__)
 
 PORT = 3004
-HOST = '0.0.0.0'
+HOST = 'localhost'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
-def get_movie_by_id(stub, id):
-   return stub.GetMovieByID(id)
 @app.route("/get-movies-infos-of-selected-movies-by-userid/<userid>", methods=['GET'])
 def get_movies_infos_of_selected_movies_by_userid(userid):
-   moviesInfos = json.loads('{"movies": []}')
+   moviesInfos = {"movies": []}
+   booking = ""
+   userFound = False
+
    for user in users:
       if str(user["id"]) == str(userid):
-         booking = requests.get("http://172.20.190.169:3201/bookings/" + userid)
+         userFound = True
+         with grpc.insecure_channel("localhost:3003") as channel:
+            stub = booking_pb2_grpc.BookingStub(channel)
 
-   if not 'booking' in locals():
+            userid = booking_pb2.BookingUserId(userid=user["id"])
+            booking = stub.GetBookingByUserid(userid)
+
+   if not userFound:
       return make_response(jsonify({"error": "user for this userid not found"}), 400)
 
-   if booking.status_code != 200:
+   if not str(booking):
       return make_response(jsonify({"error": "this userid doesn't have bookings"}), 400)
 
    with grpc.insecure_channel('localhost:3001') as channel:
       stub = movie_pb2_grpc.MovieStub(channel)
 
-      for date in booking.json()["dates"]:
-         for movieid in date["movies"]:
+      bookingJson = json.loads(MessageToJson(booking))
+      for date in bookingJson["dates"]:
+         for movieid in date["movieid"]:
             id = movie_pb2.MovieID(id=movieid)
-            movie = get_movie_by_id(stub, id)
+            movie = stub.GetMovieByID(id)
             obj = {
                "id" : movie.id,
                "title" : movie.title,
@@ -43,7 +52,6 @@ def get_movies_infos_of_selected_movies_by_userid(userid):
                "director" : movie.director
             }
             moviesInfos["movies"].append(obj)
-   channel.close()
 
    return make_response(jsonify(moviesInfos), 200)
 
